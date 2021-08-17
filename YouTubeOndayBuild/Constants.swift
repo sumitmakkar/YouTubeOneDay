a032fe2c5db54ccc678878b86fbf97d98216b31b
Sumit <makkarsumit1986@gmail.com>
2021-08-17 20:52:39 +0530

    Constants.swift unstashed

---
+++ YouTubeOndayBuild/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  YouTubeOndayBuild
+//
+//  Created by Sumit Makkar on 17/08/21.
+//
+
+import Foundation
+
+
+struct Constants
+{
+    static let API_KEY     = "AIzaSyCObYwZBX5fllaJEaQGrPk7SAQBod5Okes"
+    static let PLAYLIST_ID = "PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb"
+    static let API_URL     = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
